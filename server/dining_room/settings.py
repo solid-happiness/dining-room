1e642b940b3e799cf880e40e07c249213f147682
ituman <van.tum@mail.ru>
2019-03-20 20:41:10 +0300

    Добавил БД postgres

--- server/dining_room/settings.py
+++ server/dining_room/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dining_room.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'solid_happines_db',
+        'USER': 'solid_happines',
+        'PASSWORD': 'qwerty',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
