c68ca895a85e2aee96f68c33afcdc6a60f59d6b3
ituman <van.tum@mail.ru>
2019-03-20 13:50:11 +0300

    Добавил комментарии в модель блюда.

--- server/dish/models.py
+++ server/dish/models.py
@@ -5,11 +5,32 @@ from django.db import models
 
 
 class Dish(models.Model):
-    name = models.CharField('Название блюда', max_length=512)
-    proteins = models.FloatField('Белки (на 100г.)', default=0)
-    fats = models.FloatField('Жиры (на 100г.)', default=0)
-    carbohydrates = models.FloatField('Углеводы (на 100г.)', default=0)
-    calorific = models.IntegerField('Калорийность (на 100г.)', default=0)
+    """
+    Модель блюда.
+    Модель вынесена в отдельное приложение для возможности дальнейщей кастомизации создания блюда.
+        Например составлять блюдо из продуктов и тд.
+    """
+    name = models.CharField(
+        'Название блюда',
+        max_length=512,
+    )
+    proteins = models.FloatField(
+        'Белки (на 100г.)',
+        default=0,
+    )
+    fats = models.FloatField(
+        'Жиры (на 100г.)',
+        default=0,
+    )
+    carbohydrates = models.FloatField(
+        'Углеводы (на 100г.)',
+        default=0,
+    )
+    calorific = models.IntegerField(
+        'Калорийность (на 100г.)',
+        help_text='В калориях (не ККал!).',
+        default=0,
+    )
 
     def __str__(self):
         return self.name
